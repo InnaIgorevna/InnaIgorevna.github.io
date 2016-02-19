40190f9bda44950118b193dccaabe73c42553f9d
InnaIgorevna <innaigorevnad@gmail.com>
2016-02-19 19:57:48 +0600

    Переделала картинки, поправила резину

---
+++ projects/technomart/js/bookmark.js
@@ -0,0 +1,18 @@
+var bookmarkForm = document.querySelector(".bookmark-modal");
+var bookmark = document.querySelectorAll(".share");
+var bookmarkFormClose = document.querySelector(".bookmark-close");
+var bookmarkFormCancel = document.querySelector(".bookmark-form-btn .btn-white");
+for(var i = 0; i < bookmark.length; i++){		
+	bookmark[i].addEventListener("click", function(event){
+		event.preventDefault();
+		bookmarkForm.classList.remove("hidden");
+	});
+}
+bookmarkFormClose.addEventListener("click", function(event){
+	event.preventDefault();
+	bookmarkForm.classList.add("hidden");
+});
+bookmarkFormCancel.addEventListener("click", function(event){
+	event.preventDefault();
+	bookmarkForm.classList.add("hidden");
+});
